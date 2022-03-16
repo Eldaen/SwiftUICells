c73de00f9c6f0fb8279db4944527888ad40f0e78
Sizov Denis <sv_skatch@bk.ru>
2022-03-16 11:35:58 +0300

    Initial Commit

---
+++ SwiftUICells/SwiftUICellsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICellsApp.swift
+//  SwiftUICells
+//
+//  Created by Денис Сизов on 16.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICellsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
